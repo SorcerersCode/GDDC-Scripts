d34180685214d391440e37bd47d78dac42b45c33
SorcerersCode <123514682+SorcerersCode@users.noreply.github.com>
2023-09-19 01:01:10 -0400

    Unneeded file

--- Source_Code/InTheWalls.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Detect the operating system
-if [[ $(uname -s) == "Linux" ]]; then
-    ./open_url_linux
-else if [[ $(uname -s) == "Windows" ]]; then 
-  
-    echo "Unsupported operating system."
-fi
