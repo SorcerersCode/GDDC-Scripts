aa04f5b31990369962548aab7bbb8c0ff6ab86e0
SorcerersCode <dtarace@albany.edu>
2023-09-18 23:36:45 -0400

    First test

--- walls.c
+++ walls.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
+char *url = "https://www.youtube.com/watch?v=z6nngId317g";
+
 void runOnWindws(){
-// Find different web browsers in system32 file
+    // Use the windows API to open up the default web browser on the computer to the youtube URL
+    ShellExecute(NULL, "open", url, NULL, NULL, SW_SHOWNORMAL);
 }
 
 void runOnLinux(){
-// Find different web browsers in file structure
+// Use the system's function to use the default web browser
+char command[256];
+    snprintf(command, sizeof(command), "xdg-open \"%s\"", url);
+    system(command);
 }
-
 
 void runOnUnix(){
-// Find different web browsers in file structure
+// Same command as the Linux system
+char command[256];
+    snprintf(command, sizeof(command), "xdg-open \"%s\"", url);
+    system(command);
 }
-
-
 
 
 // Determines the OS by the Macros on compilation and goes from there
@@ -25,7 +31,7 @@ int main() {
         printf("Operating System: Linux\n");
 	runOnLinux();
     #elif __unix__
-        printf("Operating System: Unix-like\n");
+        printf("Operating System: Unix\n");
 	runOnUnix();
     #else
         printf("Operating System: Unknown\n");
@@ -37,5 +43,3 @@ int main() {
 
 // YouTube link: 
 // https://www.youtube.com/watch?v=z6nngId317g
-
-
